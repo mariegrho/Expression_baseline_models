improved metrics calculation, corrected model error

model/basic_model.py
@@ -33,6 +33,31 @@ def prepare_dataset(gene_id, dataset, t_end):
 
     return obs
 
+def gof_evaluation(idata, gene_id, model, dataset, out_path):
+
+    non_nan_times = idata.observed_data.y.time.values[~np.isnan(idata.observed_data.y.values)]
+    obs = idata.observed_data.y.sel(time=non_nan_times).values
+    pred = idata.posterior_model_fits.y.mean(dim=("chain","draw")).sel(time=non_nan_times).values
+
+    metrics = pd.DataFrame(columns=["gene_id", "model", "BIC", "rho", "NRMSE", "MASE"])
+
+    rho = spearman_correlation(obs, pred)
+    nrmse = calc_nrmse(obs, pred)[0]         # by Range
+    accepted = (rho > 0.7) & (nrmse < 0.2)
+
+    row = []
+    row.append({
+        "gene_id":gene_id,
+        "model":model,
+        "dataset": dataset,
+        "BIC": calc_bic(idata),
+        "rho": rho,
+        "NRMSE": nrmse, 
+        "MASE": calc_mase(obs, pred),
+        "accepted": accepted,
+    })
+    pd.DataFrame(row).to_csv(out_path, index=False)
+
 def basic_1s(t, M0, beta, delta):
     '''
     beta: transcription rate
@@ -55,7 +80,7 @@ def main(gene_id, dataset, kernel="nuts", t_end=120, plot=True, smooth=False, sk
     model = "Basic"
     sim.model = basic_1s
 
-    sim.config.case_study.name = f"{model}/{dataset}_{t_end}"
+    sim.config.case_study.name = f"{t_end}_hpf/{model}/{dataset}"
     sim.config.case_study.scenario = gene_id
     
     
@@ -157,6 +182,9 @@ def main(gene_id, dataset, kernel="nuts", t_end=120, plot=True, smooth=False, sk
     sim.report()
     #sim.save_observations(force=True)
     sim.config.save(force=True)
+
+    # evaluation of results
+    gof_evaluation(sim.inferer.idata, gene_id, "Basic", dataset, out_path=gene_output_dir)
 
     if smooth:
         sim.coordinates["time"]= np.linspace(0, t_end, 1000)

reports/report.py
@@ -21,24 +21,22 @@ def _autocorrelation(residuals, lag=1):
     """This uses unaggregated data residuals[id,time]"""
     return pd.Series(residuals).autocorr(lag=lag)
 
-def spearman_correlation(idata):
+def spearman_correlation(obs, pred):
     '''pattern accuracy'''
     from scipy.stats import spearmanr
-    obs = idata.observed_data.y
-    pred = idata.posterior_model_fits.y.mean(dim=("chain", "draw"))
     corr = spearmanr(obs, pred)[0]
     return float(corr)
 
-def calc_nrmse(y_true, y_pred):
+def calc_nrmse(obs, pred):
     '''Normalized Root Mean Square Error'''
-    rmse = np.sqrt(np.mean((y_true - y_pred)**2))
+    rmse = np.sqrt(np.mean((obs - pred)**2))
 
     # by range
-    nrmse_range = rmse / (y_true.max() - y_true.min())
+    nrmse_range = rmse / (obs.max() - obs.min())
     # by mean
-    nrmse_mean = rmse / y_true.mean()
+    nrmse_mean = rmse / obs.mean()
     # by std
-    nrmse_std = rmse / y_true.std()
+    nrmse_std = rmse / obs.std()
 
     return [nrmse_range, nrmse_mean, nrmse_std]
 
@@ -52,3 +50,17 @@ def calc_mase(obs, pred):
     mase = mae_model / mae_naive
 
     return mase
+
+def calc_bic(idata):
+    '''
+    k = free parameters
+    n = data points
+    LL = max. Log-Likelihood (sum)
+    '''
+
+    n = len(idata.posterior_model_fits.time)
+    k = len(idata.posterior.data_vars)
+    LL = idata.log_likelihood.y.mean(dim=("chain", "draw")).sum().item()
+    bic =  k * np.log(n) - 2 * LL
+    return float(bic)
+

simulate.py
@@ -4,6 +4,7 @@ import xarray as xr
 
 from initialise import *
 from model.models import *
+from reports.report import *
 
 from pymob.simulation import SimulationBase
 from pymob.sim.config import DataVariable
@@ -53,6 +54,31 @@ def prepare_dataset(gene_id, dataset, model_version, t_end):
         return combined_ds
 
 
+def gof_evaluation(idata, gene_id, model, dataset, out_path):
+
+    non_nan_times = idata.observed_data.y.time.values[~np.isnan(idata.observed_data.y.values)]
+    obs = idata.observed_data.y.sel(time=non_nan_times).values
+    pred = idata.posterior_model_fits.y.mean(dim=("chain","draw")).sel(time=non_nan_times).values
+
+    metrics = pd.DataFrame(columns=["gene_id", "model", "BIC", "rho", "NRMSE", "MASE"])
+
+    rho = spearman_correlation(obs, pred)
+    nrmse = calc_nrmse(obs, pred)[0]         # by Range
+    accepted = (rho > 0.7) & (nrmse < 0.2)
+
+    row = []
+    row.append({
+        "gene_id":gene_id,
+        "model":model,
+        "dataset": dataset,
+        "BIC": calc_bic(idata),
+        "rho": rho,
+        "NRMSE": nrmse, 
+        "MASE": calc_mase(obs, pred),
+        "accepted": accepted,
+    })
+    pd.DataFrame(row).to_csv(os.path.join(out_path, "gof_metrics.csv"), index=False)
+    
 @click.command() 
 @click.option("--gene_id", type=str, default=None,    help="Run a single gene ID (used for array jobs)")
 @click.option("--model_version", type=str, default=None,    help="Model version: Basic, ZGA_M, ZGA_Z, Rep_M, Rep_Z")
@@ -76,7 +102,7 @@ def main(gene_id, model_version, dataset, kernel="nuts", t_end=120, plot=True, s
     sim.model = model._rhs_jax
 
     # simulation setup
-    sim.config.case_study.name = f"{model.name}/{dataset}_{t_end}"
+    sim.config.case_study.name = f"{t_end}_hpf/{model.name}/{dataset}"
     sim.config.case_study.scenario = f"{gene_id}"
 
     # output directories
@@ -161,6 +187,9 @@ def main(gene_id, model_version, dataset, kernel="nuts", t_end=120, plot=True, s
     sim.report()
     sim.config.save(force=True)
 
+    # evaluation of results
+    gof_evaluation(sim.inferer.idata, gene_id, model_version, dataset, out_path=gene_output_dir)
+
     if smooth:
         sim.coordinates["time"]= np.linspace(0, t_end, 1000)
         sim.dispatch_constructor()
@@ -170,7 +199,6 @@ def main(gene_id, model_version, dataset, kernel="nuts", t_end=120, plot=True, s
     if plot:
         from model.plots import plot_model_results
         plot_model_results(sim.inferer.idata, gene_id, model_version, path=gene_output_dir)
-    
 
 
 if __name__ == "__main__":